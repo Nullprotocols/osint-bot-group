0cceae4b08f3a6f30c65c127b273b9a8d761892f
Shahid Ansari <sahidansari3739@gmail.com>
2026-03-06 15:04:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -60,13 +60,13 @@ GLOBAL_BLACKLIST = [
 # Har command ka URL, parameter description, log channel ID, description, extra blacklist
 COMMANDS = {
     "num": {
-        "url": "https://api.subhxcosmo.in/api?key=SATYAM2&type=mobile&term={}",
+        "url": "https://zionix.xo.je/znnum?number={}",
         "param": "10-digit number",
         "log": LOG_CHANNELS["num"],
         "desc": "Phone number basic lookup",
         "extra_blacklist": [
-            "Ruk ja bhencho itne m kya unlimited request lega?? Paid lena h to bolo 100-400₹ @Simpleguy444.",
-            "owner"
+            "API POWERED BY @FRAPPEASH",
+            "_powered_by"
         ]
     },
     "tg2num": {
