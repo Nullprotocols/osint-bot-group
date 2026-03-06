988cd5791921e60cb03d0e063950ff52ffe9a296
Shahid Ansari <sahidansari3739@gmail.com>
2026-03-07 01:19:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -65,13 +65,7 @@ COMMANDS = {
         "log": LOG_CHANNELS["num"],
         "desc": "Phone number basic lookup",
         "extra_blacklist": ['owner', 
-        'https://t.me/SUBHXCOSMO \n BUY INSTANT CHEAP PRICE', 
-        '"https://t.me/SUBHXCOSMO"', 
-        '"BUY INSTANT CHEAP PRICE"',
-        '"https://t.me/SUBHXCOSMO \n BUY INSTANT CHEAP PRICE"', 
-        'https://t.me/SUBHXCOSMO', 
-        '\n',  
-        'BUY INSTANT CHEAP PRICE'
+                            'https://t.me/SUBHXCOSMO \n BUY INSTANT CHEAP PRICE'
                            ]
     },
     "tg2num": {
